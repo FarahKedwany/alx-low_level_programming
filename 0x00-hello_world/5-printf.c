bb3067036ae4b8c3e64f480a4b6392a73c2ec84e
Farah <farahkedwany@gmail.com>
2023-04-20 19:35:14 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - prints "with proper grammer.."
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n);
+
+	return(0);
+}
