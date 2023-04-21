da59098969c7cb7c55f2682aa2f86b8572106807
Farah <farahkedwany@gmail.com>
2023-04-20 19:11:53 -0700

    trial 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main(void)
-/*This program uses puts*/
+/**
+ * main - prints "Programming is like building
+ *
+ * Return: Always 0.
+ */
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
