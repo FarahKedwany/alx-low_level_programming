c021ea3857ce5373a2bde671e337040d264d4d69
Farah <farahkedwany@gmail.com>
2023-04-21 18:12:22 -0700

    trial 3

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-
 /**
  * main - description.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char letter;
-
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar (letter);
-
-	putchar('\n');
-
-	return (0);
-	
+        char letter;
+        for (letter = 'z'; letter >= 'a'; letter--)
+                putchar (letter);
+        putchar('\n');
+        return (0);
 }
