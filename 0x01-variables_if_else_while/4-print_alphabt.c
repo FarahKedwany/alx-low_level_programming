07e4114a87c25fb95585c2310e3b1576b8ed357b
Farah <farahkedwany@gmail.com>
2023-04-21 17:48:32 -0700

    trial 2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter =! 'e' && letter =! 'q')
-			putchar (letter);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
 
-	putchar ('\n')
+	putchar('\n');
 
 	return (0);
 }
