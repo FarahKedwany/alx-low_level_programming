9fbe02e9293e57e71469ea97ed76f2ca529233ad
Farah <farahkedwany@gmail.com>
2023-04-21 17:39:58 -0700

    trial 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,17 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar (letter);
-	for (letter = 'A'; letter <= 'z'; letter++)
-		putchar (letter);
-	putchar ('\n');
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
 
 	return (0);
 }
