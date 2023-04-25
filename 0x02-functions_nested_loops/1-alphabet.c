eee46c3484a42b207479f83eb6b2d310f6e358d3
Farah <farahkedwany@gmail.com>
2023-04-25 14:15:55 -0700

    alpha trial 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - description.
+ *
+ * Return: Alwways 0.
+*/
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
