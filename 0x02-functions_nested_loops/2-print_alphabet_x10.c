b649ca3cbe6298fb76b36be87525288962e60aec
Farah <farahkedwany@gmail.com>
2023-04-25 15:04:09 -0700

    trial 4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 {
 	char letter;
 	int i = 0;
-	while ((i <= 10))
+	while (i <= 10)
        	{
-		for (letter = 'a'; letter = 'z'; letter++)
+		for ((letter = 'a'; letter = 'z'; letter++))
 			putchar (letter);
 	}
      i++;
