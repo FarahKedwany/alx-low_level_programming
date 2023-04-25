0b724c847214b6b5b8253ba27e9a1b9ec2c28f2e
Farah <farahkedwany@gmail.com>
2023-04-25 14:46:32 -0700

    trial 13

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,8 +10,8 @@ void print_alphabet(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar (letter);
 
-	putchar('\n');
+	putchar ('\n');
 }
