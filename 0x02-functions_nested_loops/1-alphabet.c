c2bcd85b189cf3539be12c8ce375bc3695cb9bd7
Farah <farahkedwany@gmail.com>
2023-04-25 14:38:48 -0700

    trial 8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,6 @@
 #include <stdio.h>
 
 /**
- * main - Entry point.
- *
- * Return: Always 0.
- *
-*/
-
-int main(void)
-{
-
-/**
  * print_alphabet - description.
  *
 */
@@ -21,8 +11,7 @@ void print_alphabet(void)
 	char letter;
 
 	for(letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
+		_putchar(letter);
+
+	_putchar('\n');
 }
-   return (0);
-   }
