927ec53eb47d62be9746892e44a59bd367f85b0c
Farah <Farahkedwany@gmail.com>
2023-04-26 16:07:15 +0000

    trial 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - tests function
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+
+	n = -50;
+	positive_or_negative(n);
+
+	return (0);
+
+
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - says if integer is positive or negative.
+ * Return: Always 0.
+*/
+int positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
