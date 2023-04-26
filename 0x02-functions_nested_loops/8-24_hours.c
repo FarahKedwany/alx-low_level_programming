8005df6945ada299f362a591eaf4485298e6e9dc
Farah <Farahkedwany@gmail.com>
2023-04-26 01:28:17 +0000

    jack 1

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour == 0; hour <= 23; hour==)
+	{
+		for (minute = 0; minute <= 59; minute++)
+			putchar('hour:minute');
+		}
+}
