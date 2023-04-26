293960eacf3a0010ce1041ff8365870ebd9e2c0c
Farah <farahkedwany@gmail.com>
2023-04-25 17:02:01 -0700

    sign trial 1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: int to be declared.
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar ('+');
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		putchar ('-');
+		return (-1);
+	}
+
+	else
+	{
+		putchar ('0');
+		return (0);
+	}
+
+
+}
