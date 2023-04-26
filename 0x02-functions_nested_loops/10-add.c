55656dfff5812f7eed759d8fba18d83822d34b1c
Farah <Farahkedwany@gmail.com>
2023-04-26 01:47:41 +0000

    trial 1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers.
+ * @i: to be declared
+ * @n: to be declared
+ *
+ * Return: result of add.
+*/
+
+int add(int i, int n)
+{
+	return (i + n);
+}
