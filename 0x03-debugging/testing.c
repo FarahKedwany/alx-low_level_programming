c5a41680dd5a56ad06fb9dbbd225eb642941e0de
Farah <Farahkedwany@gmail.com>
2023-04-26 17:07:36 +0000

    trial to fix main.h

---
+++ 0x03-debugging/testing.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Description
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+
+	return (0);
+}
