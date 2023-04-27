2ff474724911d1dcf899b063c65b20005e143de4
Farah <Farahkedwany@gmail.com>
2023-04-27 13:51:09 +0000

    trial 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,29 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 && year % 100 == 0) && !(year % 400 == 0))
+    if ((year % 4 == 0 && year % 100 == 0) && (year % 400 == 0))
     {
         if ((month >= 1 && month <= 12) && (day >= 1 && day <= 31))
-        {
-            printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-
-        }
-
+        {	
+		printf("Day of the year: %d\n", day);
+          	printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		printf("invalid date: %02d/%02d/%04d\n", month, day, year);
+	}
     }
     else
     {
         if ((month < 1 || month > 12) || (day < 1 || day > 31))
         {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
         }
-        else if ((month >= 1 && month <= 12) && (day >= 1 && day <= 31))
+        else
         {
             printf("Day of the year: %d\n", day);
             printf("Remaining days: %d\n", 365 - day);
         }
     }
 }
+
