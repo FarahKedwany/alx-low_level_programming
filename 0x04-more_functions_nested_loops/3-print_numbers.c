c8ed091755435272bccb47a01b2449fcf0d03979
Farah <Farahkedwany@gmail.com>
2023-04-27 18:56:40 +0000

    trying

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(void)
 
         for (num = '0'; num <= '9'; num++)
         {
-                putchar (num);
+                putchar (num + '0');
         }
 
         putchar ('\n');
