f7b1194b4822d6bbe7f002e9261a5010b05f7f48
Farah <Farahkedwany@gmail.com>
2023-04-27 18:51:53 +0000

    making code betty-compliant

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,16 @@
  * print_numbers - prints numbers from 0 to 9
  * followed by a new line.
  *
-*/
+ */
 
 void print_numbers(void)
 {
         int num;
 
         for (num = '0'; num <= '9'; num++)
-                putchar (num);
+        {
+                putchar(num);
+        }
 
-        putchar ('\n');
+        putchar('\n');
 }
