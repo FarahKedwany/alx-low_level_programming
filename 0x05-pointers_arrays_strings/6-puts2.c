083e72b2d67ed7c4b54649af4ca619307b9ace04
Farah <Farahkedwany@gmail.com>
2023-05-02 20:39:03 +0000

    removing whitespace

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * puts2 - prints every other character of a string, 
- * starting with the first character, 
+ * puts2 - prints every other character of a string,
+ * starting with the first character,
  * followed by a new line.
  * @str: string.
  *
