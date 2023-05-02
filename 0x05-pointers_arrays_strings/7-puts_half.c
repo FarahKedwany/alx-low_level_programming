d63a9929b40d3929c9a0f9db32cab88744aa195a
Farah <farahkedwany@gmail.com>
2023-05-02 14:37:12 -0700

    removing indetations

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,18 +9,18 @@
 
 void puts_half(char *str)
 {
-		int length = 0;
-		int n, i;
+	int length = 0;
+	int n, i;
 
-		while (str[length] != '\0')
-		{
-				length++;
-		}
-		n = (length - 1) / 2;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	n = (length - 1) / 2;
 
-		for (i = n + 1; str[i] != '\0'; i++)
-		{
-				_putchar(str[i]);
-		}
-		_putchar('\n');
-}	
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
