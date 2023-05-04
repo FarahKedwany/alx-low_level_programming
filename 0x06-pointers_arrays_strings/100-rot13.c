ed4523c611ad4cb976f2706e482c6580ce34c4f5
Farah <Farahkedwany@gmail.com>
2023-05-04 23:31:29 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @rot: string
+ *
+ * Return: string in rot13
+*/
+
+char *rot13(char *rot)
+{
+	int i;
+	int j;
+	char before[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char after[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijkl";
+
+	for (i = 0; rot[i] != '\0'; i++)
+	{
+		for (j = 0; before[j] != '0'; j++)
+		{
+			if (rot[i] == before[j])
+			{
+				rot[i] = after[j];
+				break;
+			}
+		}
+	}
+	rot[i] = '\0';
+	return (rot);
+}
