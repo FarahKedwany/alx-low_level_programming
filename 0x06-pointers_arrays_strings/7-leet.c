67b1cdb60acc9c26e76b57efd1d5127d108062a0
Farah <Farahkedwany@gmail.com>
2023-05-04 23:14:05 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337.
+ * @str: string
+ *
+ * Return: encoded string
+*/
+
+char *leet(char *str)
+{
+	int i;
+	int j;
+	char leet[] = {'4','3','0','7','1','\0'};
+	char noleet[] = {'a', 'e','o','t','l','\0'};
+	char nooleet[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == noleet[j] || str[i] == nooleet[j])
+			{
+				str[i] = leet[j];
+			}
+		}
+	}
+	return (str);
+}
+
