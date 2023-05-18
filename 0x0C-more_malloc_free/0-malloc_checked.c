eafcbae660d3855d3c2e99f1d311fb7c968804f7
Farah <Farahkedwany@gmail.com>
2023-05-18 23:50:04 +0000

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - Description.
+ * @b: to be declared.
+ *
+ * Return: returns a pointer to allocated memory,
+ * if malloc fails, the malloc_checked function should 
+ * cause normal process termination 
+ * with a status value of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
